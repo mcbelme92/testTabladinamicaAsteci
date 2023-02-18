9aa92024c9979eac5aa795d99e22fd8409d97045
pepe telleznewlap <mcbelme92@gmail.com>
2023-02-17 18:46:04 -0600

    first commit tablaasteci4

--- src/test/TableDynamic.test.js
+++ src/test/TableDynamic.test.js
@@ -4,8 +4,8 @@ describe('Pruebas en </>', () => {
     
   test('debe de retornar un arreglo de objetos',async () => {
       const informacion = [];
-      const informacionFuncion =await getInformationGob(informacion);
-      console.log(informacionFuncion)
+      const informacionFuncion =await getInformationGob();
+      //console.log(informacionFuncion)
 
    });
 
